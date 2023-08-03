10c80be12b2b4ddf2bbf949dc289d1e0ac782e62
Barath-K-R <132824019+Barath-K-R@users.noreply.github.com>
2023-08-03 11:27:54 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,36 @@
+const people = [
+  {
+    id: 1,
+    image: 'https://images.unsplash.com/photo-1525134479668-1bee5c7c6845?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fHww&w=1000&q=80',
+    name: 'maria ferguson',
+    title: 'office manager',
+    quote:
+      'Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.',
+  },
+  {
+    id: 2,
+    image: 'https://img.freepik.com/free-photo/portrait-hesitant-man-purses-lips-looks-bewilderment-feels-doubt_273609-16785.jpg',
+    name: 'john doe',
+    title: 'regular guy',
+    quote:
+      'Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.',
+  },
+  {
+    id: 3,
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRk4OL8UuR6R8hGh6d4VQbRJh7znmM8BykmBTEmysE&s',
+    name: 'peter smith',
+    title: 'product designer',
+    quote:
+      'Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.',
+  },
+  {
+    id: 4,
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1_LsXvnk7Eq1C4B7cBAK_anNTescCkvVDeQ&usqp=CAU',
+    name: 'susan andersen',
+    title: 'the boss',
+    quote:
+      'Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ',
+  },
+];
+
+export default people;
